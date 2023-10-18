2b40863eacc3f746175550571f62de735839485f
amora7741 <mora7741@gmail.com>
2023-10-17 19:12:40 -0700

    Connected to flickr API

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -2,7 +2,17 @@ package com.bignerdranch.android.photogallery.api
 
 import retrofit2.http.GET
 
+private const val API_KEY = "7bb253550270a5f55bc617bc0984c72c"
+
 interface FlickrApi {
-    @GET("/")
-    suspend fun fetchContents(): String
+
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=$API_KEY" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+
+    suspend fun fetchPhotos(): String
 }
