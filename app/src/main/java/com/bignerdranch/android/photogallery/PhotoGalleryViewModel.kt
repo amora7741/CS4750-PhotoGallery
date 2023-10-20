ab14f30f6a55d50bc4036fa522e8677277b50834
amora7741 <mora7741@gmail.com>
2023-10-19 18:18:44 -0700

    Save last search query results

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
@@ -7,11 +7,14 @@ import com.bignerdranch.android.photogallery.api.GalleryItem
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 private const val TAG = "PhotoGalleryViewModel"
 class PhotoGalleryViewModel : ViewModel() {
     private val photoRepository = PhotoRepository()
+    private val preferencesRepository = PreferencesRepository.get()
     private val _galleryItems: MutableStateFlow<List<GalleryItem>> =
         MutableStateFlow(emptyList())
     val galleryItems: StateFlow<List<GalleryItem>>
@@ -19,17 +22,19 @@ class PhotoGalleryViewModel : ViewModel() {
 
     init {
         viewModelScope.launch {
-            try {
-                val items = fetchGalleryItems("bicycle")
-                _galleryItems.value = items
-            } catch (ex: Exception) {
+            preferencesRepository.storedQuery.collectLatest { storedQuery ->
+                try {
+                    val items = fetchGalleryItems(storedQuery)
+                    _galleryItems.value = items
+                } catch (ex: Exception) {
                 Log.e(TAG, "Failed to fetch gallery items", ex)
+                }
             }
         }
     }
 
     fun setQuery(query: String) {
-        viewModelScope.launch { _galleryItems.value = fetchGalleryItems(query) }
+        viewModelScope.launch { preferencesRepository.setStoredQuery(query) }
     }
     private suspend fun fetchGalleryItems(query: String): List<GalleryItem> {
         return if (query.isNotEmpty()) {
